b29789a56b9eea80adc981525f3bd0b31fa51ee5
pschaaf <pschaaf@guidewire.com>
2016-04-23 22:53:52 -0700

    Cleaner display for colors, updated include libraries in irbirc

--- bin/colors.rb
+++ bin/colors.rb
@@ -1,119 +1,152 @@
 #!/usr/bin/env ruby
 
-def getColorString(code, text=nil, pad=4)
-  text = code.to_s '' if (text == nil)
-  pad = 2 if code == 8
-  answer = sprintf "[%sm%#{pad}s[0m", code, text
-  answer += '* ' if code == 8 # do not format the footnote
-  return answer
+require 'matrix'
+
+GridSize = 6
+
+def getColorString(code, text=code, pad=4)
+  return sprintf "[%sm%#{pad}s[0m", code, text
 end
 
 def printColorString(code, text=code, pad=4)
-  print(getColorString code, text, pad)
-end
-
-GridSize = 6
-def printColorsForPage(pageName, ground)
-  puts "\n", pageName
-  line = ''
-  inverseLine = line.dup
-  (16..255).each {|color|
-    colorStr = color < 0 ? '' : color.to_s
-    line        += getColorString("40;#{ground};5;#{color}", colorStr)
-    inverseLine += getColorString("97;107;#{ground};5;#{color}", colorStr)
-    if (color == 255) or ((color-15) % 6 == 0)
-      print '  ', line, '  ', inverseLine, "\n"
-      line, inverseLine = '', ''
-    end
-  }
+  printf getColorString code, text, pad
 end
 
 def section(header)
   puts
   printColorString 4, header
-  puts
+  puts "\n\n"
   yield
-  puts "\n"
+  puts
 end
 
-def printSample(code, desc)
-  print "  echo \"\\e[#{code}m"
-  printColorString code, desc
-  puts '"'
-end
-
-section("EFFECTS") {
-  effects = %w{-unset\ all- Bold [Dim] [Italic] Underline Blink [Fast\ blink] Inverse [Concealed] [Strikeout]}
+section('EFFECTS') {
+  effects = %w{-unset\ all- Bold [Dim] [Italic] Underline Blink [Fast\ blink] Inverse [Concealed[0m]*  [Strikeout]}
+  pad = effects.max {|e| e.length}.length + 1
   (0...effects.length).each {|effect|
-    puts if effect % 4 == 0
     print "  ", (effect).to_s, ": "
-    printColorString effect, effects[effect], -13
+    printColorString effect, effects[effect], -pad
+    puts if (effect+1) % 4 == 0
   }
   puts <<STRING
 
 
-  * Bracketed items are not widely supported. The text in code #8 may not
-    be visible because it is "concealed".
+  * The text in code #8 ought to be invisible as it is "concealed".
+
+  Bracketed effects are not widely supported.
 
   Usage:  echo "\\e[<number>mHello world"
     To unset an individual effect add 20 to the effect number.
 STRING
 }
 
-section("8-COLOR PALETTE") {
+section('8-COLOR PALETTE (30..37, 90..97, 40..47, 100..107))') {
   { 0 => 'Foreground', 10 => 'Background' }.each {|ground, groundName|
-    print "\n  ", groundName
     { 0 => 'Dim', 60 => 'Bright' }.each {|intensity, intensityName|
-      printf "\n    %-6s  ", intensityName
-      (30..37).each {|color| printColorString ground+color+intensity}
-    }
-    puts
-  }
-  puts "\n  Usage:         echo \"\\e[<number>mHello world\"' "
-}
-
-section("256-COLOR PALETTE (16..231)") {
-  (0...GridSize).each {|section|
-    (0...GridSize).each {|row|
-      { 0 => 'Foreground', 10 => 'Background' }.each {|ground, groundName|
-        print '  '
-        (16..21).each {|cell|
-          color = cell + (row + (section*GridSize))*GridSize
-          printColorString "#{38+ground};5;#{color}", color
-        }
+      printf '  %-7s ', intensityName
+      (30..37).each {|cell|
+        color = ground + intensity + cell
+        printColorString color
       }
       puts
     }
+    puts
   }
-  puts <<STRING
-
-  Foreground (use 'echo "\\e[38;5;<number>mHello world"')
-  Background (use 'echo "\\e[48;5;<number>mHello world"')
-STRING
+  puts "  Usage:  echo \"\\e[<number>mHello world\"' "
 }
 
-section("GRAYSCALE") {
-  (0..3).each {|row|
-    { 0 => 'Foreground', 10 => 'Background' }.each {|ground, groundName|
-      print '  '
+section('256-COLOR PALETTE (16..255)') {
+  [38, 48].each {|ground|
+    rowCounts = (0...GridSize).to_a
+    (0..2).each {|section|
+      rowCounts.each {|row|
+        print '  '
+        [16, 22, 28, 34, 40, 46, 82, 76, 70, 64, 58, 52].each {|cell|
+          color = cell + row + (72*section)
+          colorCode = "#{ground};5;#{color}"
+          colorCode = '30;' + colorCode if [40, 46, 82, 76].include?(cell)
+          printColorString colorCode, color
+        }
+       puts
+      }
+      rowCounts = rowCounts.reverse
+    }
+    print '  '
+    (0..3).each {|row|
       (232..237).each {|cell|
         color = cell + (row*GridSize)
-        printColorString "#{38+ground};5;#{color}", color
+        printColorString "#{ground};5;#{color}", color
       }
+      print "\n  " if row % 2 == 1
     }
     puts
   }
-  puts "\n  Usage:         echo \"\\e[<number>mHello world\"' "
+
+  puts <<STRING
+  Usage:
+    Foreground 'echo "\\e[38;5;<number>mHello world"'
+    Background 'echo "\\e[48;5;<number>mHello world"'
+STRING
 }
 
-section("USAGE") {
-  puts "\n  echo \"\\e[<effect*>;<foreground_color>;<background_color>mHello World\\\!\e[0m\""
+def printSamples(codeToDesc)
+  codeToDesc.each {|code, desc|
+    print "  echo \"\\e[#{code}m"
+    printColorString code, desc
+    puts '"'
+  }
+end
 
-  printSample '31', 'Red foreground'
-#  printSample '38;5;1', 'Red foreground'
-  printSample '48;5;12', 'Light blue background'
-  printSample '38;5;94', 'Brown foreground'
-  printSample '1;38;5;11', 'Boldface gold foreground'
-  printSample '1;4;38;5;55', 'Boldface underlined purple foreground'
-  printSample '38;5;171;48;5;11', 'Pink foreground, gold background'
+section('USAGE') {
+  puts '  Begin your text with "\e[<effect*>;<foreground>;<background>m"', ''
+
+  printSamples( {
+    '31'               => 'Red foreground',
+    '48;5;12'          => 'Light blue background',
+    '38;5;94'          => 'Brown foreground',
+    '1;38;5;11'        => 'Boldface gold foreground',
+    '1;4;38;5;55'      => 'Boldface underlined purple foreground',
+    '38;5;171;48;5;11' => 'Pink foreground, gold background'
+  })
 }
+
+showColor = ARGV[0] != '-n'
+
+#
+# CONSTRUCT THE VALUES
+#
+# sections = []
+# [38, 48].each {|ground|
+#   (0...GridSize).each {|sectionNum|
+#     section = []
+#     (0...GridSize).each {|rowNum|
+#       row = []
+#       (0...GridSize).each {|cellNum|
+#         color = 16 + cellNum + (GridSize * (rowNum + (sectionNum*GridSize)))
+#         colorCode = "#{ground};5;#{color}"
+#         colorCode += ';0' if not showColor
+#         row.push(getColorString colorCode, color)
+#       }
+#       # if sectionNum % 2 == 1 then
+#       #   section.unshift(row)
+#       # else
+#         section.push(row)
+#       # end
+#     }
+#     sections.push((section))
+#   }
+# }
+#sections = Matrix[*sections]
+
+#
+# DISPLAY THE VALUES
+#
+
+# Naive linear
+# sections.each_with_index {|section, sectionIndex|
+#   section.each_with_index {|cell, cellIndex|
+#     print cell.join
+#     puts if cellIndex+1 == GridSize/2
+#   }
+#   puts
+# }
