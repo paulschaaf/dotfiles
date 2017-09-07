e6fd3b944fa783a9a169da998a115519c562897a
pschaaf <pschaaf@guidewire.com>
2017-09-07 15:44:42 -0700

    .

---
+++ src/javascript/greasemonkey/IFDB Show XML Record.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         IFDB Show XML Record
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://ifdb.tads.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var link = document.createElement('a');
+    link.href = document.location.href + '&ifiction';
+    link.target = '_blank';
+    link.text = 'XML Record';
+
+    var detailsSection = document.getElementsByClassName("details")[0];
+    detailsSection.appendChild(link);
+})();
