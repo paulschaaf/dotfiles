ec10ca20c494ddf350dcf36f4bff07f9db2157fa
Paul Schaaf <paul.schaaf@gmail.com>
2019-01-06 11:20:21 -0800

    .

--- src/javascript/greasemonkey/IFDB Show XML Record.user.js
+++ src/javascript/greasemonkey/IFDB Show XML Record.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Adds a link to show the XML record to an IFDB entry
 // @author       P.G. Schaaf
-// @match        http://ifdb.tads.org/*
+// @match        http*://ifdb.tads.org/viewgame*
 // @grant        none
 // @require      file:///home/pschaaf/src/javascript/greasemonkey/IFDB Show XML Record.user.js
 // ==/UserScript==
