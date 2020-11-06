adca6b8c665f3b59c42280e6945f6a9ab957bfb2
Paul Schaaf <paul.schaaf@gmail.com>
2020-11-06 11:54:03 -0800

    .

--- etc/movein.sh
+++ etc/movein.sh
@@ -5,80 +5,78 @@
 cd $HOME
 backup=~/backup
 shell=/bin/zsh
-set -x
 if [ "$host" = "copernicus" ]; then
-    yaourt -S broadcom-wl
+   yaourt  -S broadcom-wl
 fi
 
 ## LIBRARIES AND PACKAGES ===========================================
 libraries=(
-#    libimage-exiftool-perl # edit EXIF data in image
-#    libncurses
-#    libncurses-dev
-#    libevent
+   #    libimage-exiftool-perl # edit EXIF data in image
+   #    libncurses
+   #    libncurses-dev
+   #    libevent
 )
 
 # these get installed first, in the order defined
 ordered_packages=(
-    mlocate
-    ripgrep                # quicker grep
-    google-chrome          # the best browser
-    git                    # version control
-    intellij-idea-ultimate # ide
-    tmux                   # terminal multiplexer
-    byobu                  # tmux/screen enhancements
-#    cairo-dock             # Mac-like icon dock
-#    cairo-dock-plug-ins    # Mac-like icon dock
-#    nvm                    # nodejs version manager
+   mlocate
+   ripgrep                 # quicker grep
+   google-chrome           # the best browser
+   git                     # version control
+   intellij-idea-ultimate  # ide
+   tmux                    # terminal multiplexer
+   byobu                   # tmux/screen enhancements
+   #    cairo-dock             # Mac-like icon dock
+   #    cairo-dock-plug-ins    # Mac-like icon dock
+   #    nvm                    # nodejs version manager
 )
 
 # These are installed after the ordered_packages. Keep them sorted for convenience
 unordered_packages=(
-    ascii                 # tree of ascii codes
-#    atom
-    bcompare
-    bcompare-kde5
-#    davfs2                # mount box.com into filesystem
-#    devede                # make video DVDs
-    dolphin-plugins       # git integration into KDE file manager
-    emacs                 # the king of editors
-    enscript              # convert txt to ps, html, rtf, etc.
-    inxi                  # system info tool
-    irb                   # Ruby REPL
-    jdk-dcevm             # Java Dynamic Code Evolution VM
-    kdiff3
-    keychain              # ssh key organizer
-    lsof
-#    lynx                  # command line browser
-#    manjaro-i3-solarized-settings
-#    mp3fs                 # MP3 virtualf ilesystem
-    nerd-fonts-fira-code  # programming font with ligatures
-    nmap
-#    nodejs
-#    npm
-    oracle~jdk7           # Java 1.7
-    oracle~jdk8           # Java 1.8
-#    sqlops                # MS SQL Server admin tool
-    tree
-    units                 # unit conversions
-    vlc                   # video player
-    xorg-xkill
+   ascii                  # tree of ascii codes
+   #    atom
+   bcompare
+   bcompare-kde5
+   #    davfs2                # mount box.com into filesystem
+   #    devede                # make video DVDs
+   dolphin-plugins        # git integration into KDE file manager
+   emacs                  # the king of editors
+   enscript               # convert txt to ps, html, rtf, etc.
+   inxi                   # system info tool
+   jdk-dcevm              # Java Dynamic Code Evolution VM
+   keychain               # ssh key organizer
+   lsof
+   #    lynx                  # command line browser
+   #    manjaro-i3-solarized-settings
+   #    mp3fs                 # MP3 virtualf ilesystem
+   nerd-fonts-fira-code   # programming font with ligatures
+   nmap
+   #    nodejs
+   #    npm
+   #    oracle~jdk7            # Java 1.7
+   #    oracle~jdk8            # Java 1.8
+   ruby-irb               # Ruby REPL
+   #    sqlops                # MS SQL Server admin tool
+   tree
+   units                  # unit conversions
+   vlc                    # video player
+   xorg-xkill
 )
 
 applications=(
-#    amarok                # music
-    calibre               # e-book manager
-    filezilla             # file transfers
-#    freecad
-    frostwire             # bittorrent client
-#    gitkraken
-    masterpdfeditor4
-    slack-desktop         # IM client
-    sweethome3d           # architectural modelling
-#    virtualbox            # VM manager
-#    virtualbox-ext-oracle
-#    virtualbox-guest-iso
-#    visual-studio-code
+   #    amarok                # music
+   calibre                # e-book manager
+   #    filezilla              # file transfers
+   #    freecad
+   frostwire              # bittorrent client
+   #    gitkraken
+   masterpdfeditor4
+   #    slack-desktop          # IM client
+   #    sweethome3d            # architectural modelling
+   #    virtualbox            # VM manager
+   #    virtualbox-ext-oracle
+   #    virtualbox-guest-iso
+   #    visual-studio-code
 )
 
 # Use URL query-style syntax: e.g. double-quote any phrases and replace the spaces with plusses
@@ -86,76 +84,83 @@ manually_install=(
 )
 
 git_repos=(
-    git@github.com:paulschaaf/dotfiles.git
-    git@github.com:paulschaaf/launch-url-from-string.git
+   git@github.com:paulschaaf/dotfiles.git
+   git@github.com:paulschaaf/launch-url-from-string.git
 )
 
 ## FUNCTIONS ===========================================
 dash='----------------------------------------'
 
 function printFgBg() {
-    fg=$1; shift
-    bg=$1; shift
-    echo "[38;5;${fg};48;5;${bg}m${*}[0m"
+   fg=$1
+   shift
+   bg=$1
+   shift
+   echo  "[38;5;${fg};48;5;${bg}m${*}[0m"
 }
 
 function setScreenTitle() {
-    echo -n "k$*\\"
+   echo  -n "k$*\\"
 }
 
 function h1() {
-    echo
-    printFgBg 255 21 === $*
+   echo
+   printFgBg  255 21 "=== $*"
 }
 
 function h2() {
-    setScreenTitle $*
-    echo
-    echo -n '   '
-    printFgBg 250 27 $*
+   setScreenTitle  $*
+   echo
+   echo  -n '   '
+   printFgBg  250 27 $*
 }
 
 function error() {
-    printFgBg 255 196 $*
+   printFgBg  255 196 $*
 }
 
 function backup() {
-    cp $* $backup
+   cp  $* $backup
 }
 
 function install-all() {
-    if [ `uname` = "linux" ]; then
-        eval local packages=\${${*}[@]}
-        h1 Install $* #: $packages
-        skipped=''
-    #    set -x
-        for pkg in ${packages[@]}; do
-            package=${pkg//\~/ }
-            # is it already installed?
-            if yaourt --query $package 2> /dev/null; then
-                skipped=${skipped}\'${package}\'' '
-            else
-                h2 Installing \'$package\'
-                LESS+=-"P $package" yaourt --noconfirm $package
-                [ "$?" -ne 0 ] && error Could not install \'$package\'
-            fi
-        done
-        h2 Skipped already installed $*: $skipped
-        set +x
-    fi
+   set -x
+   if  [ $(uname) = "Linux" ]; then
+#      eval   local packages=\${${*}[@]}
+      h1   Install $* #: $packages
+      skipped=''
+      #    set -x
+      for pkg in   "${@}"; do
+         package=${pkg//\~/ }
+         # is it already installed?
+         if    pacman -Q $package 2> /dev/null; then
+            skipped=${skipped}\'${package}\'' '
+         else
+            h2     Installing \'$package\'
+            LESS+=-"P $package"     yay --useask $package
+            [     "$?" -ne 0 ] && error Could not install \'$package\'
+         fi
+      done
+      [ -n "$skipped" ] && h2   Skipped already installed: $skipped
+      set   +x
+   fi
+   set  +x
 }
 
 function ln-all() {
-    for src in $*; do
-        target=.${src##*/}
-        # echo src=$src
-        # readlink $target
-        if [ "$(readlink $target)" == "$src" ]; then
-            echo '   '$target is already correct
-        else
-            ln -s $src ${target}
-        fi
-    done
+   cd
+   pwd
+
+   for src in "$@"; do
+      target=$(readlink ${src##*/})
+      # echo src=$src
+      # readlink $target
+      if [ "$target" = "$src" ]; then
+         echo '   '$target is already correct
+      else
+         ln -s $src ${target}
+      fi
+   done
 }
 
 ## ====================================================
@@ -163,19 +168,19 @@ function ln-all() {
 
 #h1 SYSTEM SETUP
 #h2 Remove ttys beyond CTRL-ALT-F1 and CTRL-ALT-F2
-file=/etc/systemd/logind.conf
-if grep -q '^#NAutoVTs=6$' $file; then
-    backup $file
-    sudo perl -pi -e "s/^#(NAutoVTs)=6$/\1=3/g" $file
-else
-    echo Already done!
-fi
+#file=/etc/systemd/logind.conf
+#if grep -q '^#NAutoVTs=6$' $file; then
+#    backup $file
+#    sudo perl -pi -e "s/^#(NAutoVTs)=6$/\1=3/g" $file
+#else
+#    echo Already done!
+#fi
 
 #h1 PACKAGE INSTALLATION
 #install-all libraries
-#install-all ordered_packages
-#install-all unordered_packages
-#install-all applications
+install-all $ordered_packages
+install-all $unordered_packages
+install-all $applications
 
 #h1 USER SETUP
 #for repo in ${git_repos[@]}; do
@@ -190,9 +195,8 @@ fi
 #    sudo chsh --shell $shell $USER
 #fi
 
-
 #h1 Setup Symlinks to RC files
-ln-all ~/dotfiles/etc/home/*[^~]
+ln-all ~/etc/home/*[^~]
 
 #h1 Setup Symlinks to ssh files
 #[ -d .ssh ] || mkdir .ssh
@@ -202,23 +206,23 @@ ln-all ~/dotfiles/etc/home/*[^~]
 #cd
 
 # Clean Up
-if rmdir $backup 2>/dev/null; then
-    h1 No backups were necessary
+if rmdir $backup 2> /dev/null; then
+   h1  No backups were necessary
 else
-    h1 Created backups in $backup
+   h1  Created backups in $backup
 fi
 
 if [ -n "$manually_install" ]; then
-    h1 Manually install the following packages:
-    echo "$manually_install"
-    for pkg in ${manually_install[@]}; do
-        echo google-chrome https://www.google.com/search?q="${pkg// //+}"+ubuntu+download
-    done
+   h1  Manually install the following packages:
+   echo  "$manually_install"
+   for pkg in  ${manually_install[@]}; do
+      echo   google-chrome https://www.google.com/search?q="${pkg// //+}"+ubuntu+download
+   done
 fi
 
 h1 Other packages
 rehash
-h2 npm yarn
+#h2 npm yarn
 # sudo npm install -g yarn
 
 setScreenTitle
