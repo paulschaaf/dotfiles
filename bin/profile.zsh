f364c2f7a31ff7b7648e3ea7e1eded26e5d9c7c0
Paul Schaaf <paul.schaaf@gmail.com>
2021-04-15 18:18:59 -0700

    fix(scripts): fixed MacOS path, more cleanup of ripgreprc

--- bin/profile.zsh
+++ bin/profile.zsh
@@ -3,7 +3,7 @@
 # source this at the start of your script
 
 # if $trace is set log everything
-(( $#trace )) && echo "\e[42m# ===== Sourcing ${(%):-%N}\e[0m" && set -x
+(( $#trace )) && set -x
 
 # if $zprof is set run the profiler
 if (( $#zprof )); then
