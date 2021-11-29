d1e4d2cb17cf9d9930373fb4a577e57cf73206f2
Paul Schaaf <pschaaf@guidewire.com>
2021-11-29 08:44:52 -0800

    fix(scripts): ported to Cygwin

--- etc/movein.sh
+++ etc/movein.sh
@@ -3,10 +3,12 @@
 # capture packages:
 # pacman -Qqen >| pkglist.txt && pacman -Qqem >| pkglist_aur.txt
 cd $HOME
+
+export PATH
 backup=~/backup
 shell=/bin/zsh
 if [ "$host" = "copernicus" ]; then
-   yaourt  -S broadcom-wl
+   yaourt -S broadcom-wl
 fi
 
 
@@ -94,65 +96,61 @@ function printFgBg() {
    shift
    bg=$1
    shift
-   echo  "[38;5;${fg};48;5;${bg}m${*}[0m"
+   echo "[38;5;${fg};48;5;${bg}m${*}[0m"
 }
 
 function setScreenTitle() {
-   echo  -n "k$*\\"
+   echo -n "k$*\\"
 }
 
 function h1() {
    echo
-   printFgBg  255 21 "=== $*"
+   printFgBg 255 21 "=== $*"
 }
 
 function h2() {
-   setScreenTitle  $*
+   setScreenTitle $*
    echo
-   echo  -n '   '
-   printFgBg  250 27 $*
+   echo -n '   '
+   printFgBg 250 27 $*
 }
 
 function error() {
-   printFgBg  255 196 $*
+   printFgBg 255 196 $*
 }
 
 function backup() {
-   cp  $* $backup
+   cp $* $backup
 }
 
 function install-all() {
-   set -x
-   if  [ $(uname) = "Linux" ]; then
-#      eval   local packages=\${${*}[@]}
-      h1   Install $* #: $packages
+   if [ $(uname) = "Linux" ]; then
+      #      eval   local packages=\${${*}[@]}
+      h1 Install $* #: $packages
       skipped=''
-      #    set -x
-      for pkg in   "${@}"; do
+      for pkg in "${@}"; do
          package=${pkg//\~/ }
          # is it already installed?
-         if    pacman -Q $package 2> /dev/null; then
+         if pacman -Q $package 2>/dev/null; then
             skipped=${skipped}\'${package}\'' '
          else
-            h2     Installing \'$package\'
-            LESS+=-"P $package"     yay --useask $package
-            [     "$?" -ne 0 ] && error Could not install \'$package\'
+            h2 Installing \'$package\'
+            LESS+=-"P $package" yay --useask $package
+            [ "$?" -ne 0 ] && error Could not install \'$package\'
          fi
       done
-      [ -n "$skipped" ] && h2   Skipped already installed: $skipped
-      set   +x
+      [ -n "$skipped" ] && h2 Skipped already installed: $skipped
    fi
-   set  +x
 }
 
 function ln-all() {
    cd
-   pwd
 
    for src in "$@"; do
       target=$(readlink ${src##*/})
-      # echo src=$src
-      # readlink $target
+      readlink $target
+      echo src=$src
+      echo target=$target
       if [ "$target" = "$src" ]; then
          echo '   '$target is already correct
       else
@@ -164,7 +162,21 @@ function ln-all() {
 ## ====================================================
 [[ -d $backup ]] || mkdir $backup
 
-#h1 SYSTEM SETUP
+h1 SYSTEM SETUP
+h2 Setup root directories
+for src in dotfiles/*(/); do
+   target=${src##*/}
+   #   echo src=$src
+   #   echo target=$target
+   if [ -e "$target" ]; then
+      echo '   Skipping existing target $target'
+   else
+      inCygwin \
+         && echo Run this as admin: mklink /D ${target} ${src} \
+         || ln -s ${src} ${target}
+   fi
+done
+
 #h2 Remove ttys beyond CTRL-ALT-F1 and CTRL-ALT-F2
 #file=/etc/systemd/logind.conf
 #if grep -q '^#NAutoVTs=6$' $file; then
@@ -187,21 +199,21 @@ git_repos=(
    git@github.com:paulschaaf/launch-url-from-string.git
 )
 for repo in ${git_repos[@]}; do
-    h1 Downloading Git Repository: $repo
-    git clone $repo
+   h1 Downloading Git Repository: $repo
+   git clone $repo
 done
-PATH=~/bin;${PATH}
+PATH=~/bin:${PATH}
 rehash
 mkdir ~/bin/macos ~/bin/linux ~/bin/win 2>/dev/null
 
 ln -fs /usr/bin/false ~/bin/inCygwin
-ln -fs /usr/bin/true  ~/bin/win/inCygwin
+ln -fs /usr/bin/true ~/bin/win/inCygwin
 
 ln -fs /usr/bin/false ~/bin/inMacOS
-ln -fs /usr/bin/true  ~/bin/macos/inMacOS
+ln -fs /usr/bin/true ~/bin/macos/inMacOS
 
 ln -fs /usr/bin/false ~/bin/inLinux
-ln -fs /usr/bin/true  ~/bin/linux/inLinux
+ln -fs /usr/bin/true ~/bin/linux/inLinux
 
 #if grep --quiet $USER:$shell /etc/passwd; then
 #    h2 Default shell already set to ${shell##*/}
@@ -210,10 +222,20 @@ ln -fs /usr/bin/true  ~/bin/linux/inLinux
 #    sudo chsh --shell $shell $USER
 #fi
 
-#h1 Setup Symlinks to RC files
-ln-all ~/etc/home/*[^~]
+h1 Setup Symlinks to RC files
+for src in etc/home/*[^~]; do
+   target=.${src##*/}
+   #   echo src=$src
+   #   echo target=$target
+   # readlink $target
+   if [ -e "$target" ]; then
+      echo '   Skipping existing target $target'
+   else
+      ln -s $src ${target}
+   fi
+done
 
-#h1 Setup Symlinks to ssh files
+h1 Setup Symlinks to ssh files
 #[ -d .ssh ] || mkdir .ssh
 #chmod 700 .ssh
 #cd .ssh
@@ -221,17 +243,17 @@ ln-all ~/etc/home/*[^~]
 #cd
 
 # Clean Up
-if rmdir $backup 2> /dev/null; then
-   h1  No backups were necessary
+if rmdir $backup 2>/dev/null; then
+   h1 No backups were necessary
 else
-   h1  Created backups in $backup
+   h1 Created backups in $backup
 fi
 
 if [ -n "$manually_install" ]; then
-   h1  Manually install the following packages:
-   echo  "$manually_install"
-   for pkg in  ${manually_install[@]}; do
-      echo   google-chrome https://www.google.com/search?q="${pkg// //+}"+ubuntu+download
+   h1 Manually install the following packages:
+   echo "$manually_install"
+   for pkg in ${manually_install[@]}; do
+      echo google-chrome https://www.google.com/search?q="${pkg// //+}"+ubuntu+download
    done
 fi
 
